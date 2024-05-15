1d26f39bb2a5e76677ce446835a59f2a8d685aee
AlexVlP <162351784+AlexVProf@users.noreply.github.com>
2024-05-15 12:43:45 +0300

    Delete app directory

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.models import ModelAlex
-import inspect
-import app.models
-
-# Register your models here.
-#admin.site.register(ModelAlex)
-ms = inspect.getmembers(app.models, inspect.isclass)
-for model in ms: admin.site.register(model[1])
